7ce7ee5be56576a1b341b7e7f1c5356a411834cf
firlans <firlansyah54321@gmail.com>
2024-06-19 11:40:27 +0700

    fix bug

--- src/app.js
+++ src/app.js
@@ -19,7 +19,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(routes);
-
+// Error handling middleware for multer and other errors
+app.use((err, req, res, next) => {
+  if (err instanceof multer.MulterError || err.message === "Not an image! Please upload an image.") {
+    return res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+  next(err);
+});
 app.listen(port, "0.0.0.0",() => {
   console.log(`server running on http://0.0.0.0:${port}`);
 });
